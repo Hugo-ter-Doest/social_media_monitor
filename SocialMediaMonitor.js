c92b375cfb411d3259411994606f613441722c1f
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2014-11-10 23:15:57 +0100

    Some work on the queue mechanism.

--- SocialMediaMonitor.js
+++ SocialMediaMonitor.js
@@ -4,12 +4,20 @@
  * and open the template in the editor.
  */
 
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=UTF-8'
-    });
-    
-    res.end('Hello from SocialMediaMonitor.\n');
-    
-}).listen(9080, "");
+//var http = require('http');
+//http.createServer(function (req, res) {
+//    res.writeHead(200, {
+//        'Content-Type': 'text/plain; charset=UTF-8'
+//    });
+//    
+//    res.end('Hello from SocialMediaMonitor.\n');
+//    
+//}).listen(9080, "");
+
+var kue = require('kue');
+var TwitterConnection = require('./lib/TwitterConnector');
+var MessageProcessor = require('MessageProcessor');
+
+message_queue = kue.createQueue();
+var twitter = new TwitterConnection(message_queue);
+var message_processor = new MessageProcessor(message_queue);
