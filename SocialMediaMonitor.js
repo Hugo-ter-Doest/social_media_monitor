97df37d1dd5e015f0f3978c2680202cb076f3b5d
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2014-11-22 23:50:14 +0100

    Two step authentication OAuth.

--- SocialMediaMonitor.js
+++ SocialMediaMonitor.js
@@ -17,13 +17,20 @@
 var kue = require('kue');
 var TwitterConnection = require('./lib/TwitterConnector');
 var InstagramConnection = require('./lib/InstagramConnector');
+var LinkedInConnection = require('./lib/LinkedInConnector');
 var MessageProcessor = require('./lib/MessageProcessor');
 
 message_queue = kue.createQueue();
 console.log("Created queue");
+
 //var twitter = new TwitterConnection(message_queue);
 //console.log("Created Twitter connection");
-var instagram = new InstagramConnection(message_queue);
-console.log("Created Instagram connection");
+
+//var instagram = new InstagramConnection(message_queue);
+//console.log("Created Instagram connection");
+
+var linkedin = new LinkedInConnection(message_queue);
+console.log("Created LinkedIn connection");
+
 var message_processor = new MessageProcessor(message_queue);
 console.log("Created message processor");
